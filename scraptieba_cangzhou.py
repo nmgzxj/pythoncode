d50c49ca83686050ef0567e208ea02a9d5d65644
King Zhang <46005466@qq.com>
2016-08-24 18:58:24 +0800

    cangzhou

--- scraptieba_cangzhou.py
+++ scraptieba_cangzhou.py
@@ -3,6 +3,7 @@ import http.client
 import bs4
 import re
 
+page_num=10
 pattern=re.compile('post_content_[0-9]{1,}')
 def IsRightId(id):
     group=pattern.findall(id)
@@ -18,13 +19,13 @@ def GetEveryPage(url,file):
     else:
         rep_utf=rep.read().decode('utf-8')
         soup=bs4.BeautifulSoup(rep_utf)
-        for div in soup.find_all('a',{"class":"j_th_tit"}):
-                file.write(div.get_text()+'\r\n')
-                print(div.get_text()+'\r\n')
+        for a in soup.find_all('a',{"class":"j_th_tit"}):
+                file.write('"'+a.get_text()+'",http://tieba.baidu.com'+a.attrs['href']+'\r\n')
+                print(a.get_text()+',http://tieba.baidu.com'+a.attrs['href']+'\r\n')
 
-url='http://tieba.baidu.com/f?ie=utf-8&kw=%E6%B2%A7%E5%B7%9E&pn='
-file_tieba=open('cangzhouba.txt','w+',encoding='utf-8')
-for i in range(1,17):
+url='http://tieba.baidu.com/f?ie=utf-8&kw=python&pn='
+file_tieba=open('pythonba.csv','w+',encoding='utf-8')
+for i in range(1,10):
     url_everypage=url+str((i-1)*50)
     print('Processing page:'+url+str(i)+str('/16......'))
     GetEveryPage(url_everypage,file_tieba)
--- scraptieba_python.py
+++ scraptieba_python.py
@@ -2,7 +2,15 @@ import urllib.request
 import http.client
 import bs4
 import re
+import sys
+import importlib
+importlib.reload(sys)
 
+page_num=10
+url='http://tieba.baidu.com/f?ie=utf-8&kw=%E6%B2%A7%E5%B7%9E'
+
+
+page_num=page_num+1
 pattern=re.compile('post_content_[0-9]{1,}')
 def IsRightId(id):
     group=pattern.findall(id)
@@ -18,15 +26,14 @@ def GetEveryPage(url,file):
     else:
         rep_utf=rep.read().decode('utf-8')
         soup=bs4.BeautifulSoup(rep_utf)
-        for div in soup.find_all('a',{"class":"j_th_tit"}):
-                file.write(div.get_text()+'\r\n')
-                print(div.get_text()+'\r\n')
+        for a in soup.find_all('a',{"class":"j_th_tit"}):
+                file.write('"'+a.get_text()+'",http://tieba.baidu.com'+a.attrs['href']+'\r\n')
+                print(a.get_text()+',http://tieba.baidu.com'+a.attrs['href']+'\n')
 
-url='http://tieba.baidu.com/f?ie=utf-8&kw=python&pn='
-file_tieba=open('pythonba.txt','w+',encoding='utf-8')
-for i in range(1,17):
-    url_everypage=url+str((i-1)*50)
-    print('Processing page:'+url+str(i)+str('/16......'))
+file_tieba=open('cangzhouba.csv','w+',encoding='utf-8')
+for i in range(1,page_num):
+    url_everypage=url+'&pn='+str((i-1)*50)
+    print('Processing page:'+url+'&pn='+str(i)+'/'+str(page_num)+'......')
     GetEveryPage(url_everypage,file_tieba)
 print('Finished!')
 file_tieba.close()
