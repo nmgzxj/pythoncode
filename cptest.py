fbd0c631cbf2165e5b6b5a6b1210ed2a1d3051ef
zhangxianjin <46005466@qq.com>
2017-10-18 17:27:38 +0800

    add GUI

--- cptest.py
+++ cptest.py
@@ -7,6 +7,15 @@ import chardet
 import time
 reload(sys)
 sys.setdefaultencoding('utf8')
+import Tkinter
+from ScrolledText import ScrolledText
+
+root = Tkinter.Tk()
+root.title('全媒声控数据收集')
+text = ScrolledText(root, font=('微软雅黑', 10))
+text.grid()  # 布局
+varl = Tkinter.StringVar()
+printTxt = ''
 
 web_name = '中国经营报'
 baseurl = "http://www.cb.com.cn"
@@ -43,23 +52,37 @@ regDetail = re.compile('<title>(.*?)</title>.*?<meta name="keywords" content="(.
 # print html
 contents = re.findall(reg, html)
 # print contents
-for link in contents:
-    url1 = baseurl+link[0]
-    details = re.findall(regDetail, crawl_content(url1))
-    print '================== '+url1+' ==============================='
-    # print details
-    #print details[0][0]
-    # break
-    print 'web_name='+web_name
-    print 'title='+details[0][0]
-    print 'url='+url1
-    print 'source='+details[0][4]
-    print 'content='+details[0][2]
-    print 'pub_time='+details[0][3].strip()
-    print 'crawl_time='+time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-    print 'keywords='+details[0][1]
-    print 'description='+details[0][2]
-    # break
+
+
+def start():
+    i = 0
+    for link in contents:
+        i = i+1
+        url1 = baseurl+link[0]
+        details = re.findall(regDetail, crawl_content(url1))
+        printTxt = '=========== '+str(i)+' ===========\n'
+        # print details
+        # print details[0][0]
+        # break
+        # print 'web_name='+web_name
+        printTxt = printTxt + 'title='+details[0][0]+'\n'
+        printTxt = printTxt + 'url='+url1+'\n'
+        printTxt = printTxt + 'source='+details[0][4]+'\n'
+        # print 'content='+details[0][2]
+        printTxt = printTxt + 'pub_time='+details[0][3].strip()+'\n'
+        printTxt = printTxt + 'crawl_time='+time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))+'\n'
+        printTxt = printTxt + 'keywords='+details[0][1]+'\n'
+        printTxt = printTxt + 'description='+details[0][2]+'\n'
+        text.insert(Tkinter.END, printTxt)
+        # break
 #print soup.get_text()
 
 
+button = Tkinter.Button(root, text='开始爬取', font=('微软雅黑', 10), command = start)
+button.grid()
+
+
+label = Tkinter.Label(root, font=('微软雅黑', 10), fg='blue', textvariable = varl)
+label.grid()
+varl.set('爬虫已准备...')
+root.mainloop()  # 进入主事件循环
