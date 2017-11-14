0d3c4f36b197991f5083c732619764cc466aa7ff
zhangxianjin <46005466@qq.com>
2017-11-14 15:28:52 +0800

    finished

--- xima1.py
+++ xima1.py
@@ -74,19 +74,23 @@ def another(url):
     ifanother = etree.HTML(html).xpath('//div[@class="pagingBar_wrapper"]/a[last()-1]/@data-page')
     if len(ifanother):
         num = ifanother[0]
-        print('本频道资源存在' + num + '个页面')
+        print('本资源存在' + num + '个页面')
         for n in range(1, int(num)):
             print('开始解析{}个中的第{}个页面'.format(num, n))
             url2 = url + '?page={}'.format(n)
             get_m4a(url2)
-        get_m4a(url)
+    get_m4a(url)
 
 def get_m4a(url):
     time.sleep(2)
     html = requests.get(url, headers=headers2).text
     soup = BeautifulSoup(html, 'lxml')
-    print soup.get("title")
+    dir = soup.find_all("title")[0].get_text()
+    down_dir = u'/users/zhangxianjin/Downloads/sound/'+dir
+    if not os.path.exists(down_dir):
+        os.mkdir(down_dir)
     numlist = etree.HTML(html).xpath('//div[@class="personal_body"]/@sound_ids')[0].split(',')
+    exists_num = 0
     for i in numlist:
         murl = 'http://www.ximalaya.com/tracks/{}.json'.format(i)
         html = requests.get(murl, headers=headers1).text
@@ -94,13 +98,20 @@ def get_m4a(url):
        # print dic
         print dic.get("title")
         print dic.get("play_path")
-        if os.path.exists('/users/zhangxianjin/Downloads/sound/%s.m4a' % dic.get("title")):
+        print down_dir
+        if os.path.exists(down_dir+'/%s.m4a' % dic.get("title")):
             print u'%s.m4a 已存在，跳过下载。' % dic.get("title")
+            exists_num = exists_num + 1
         else:
-            urllib.urlretrieve(dic.get("play_path"), '/users/zhangxianjin/Downloads/sound/%s.m4a' % dic.get("title"))
+            urllib.urlretrieve(dic.get("play_path"), down_dir+'/%s.m4a' % dic.get("title"))
+        if exists_num > 3:
+            break
         # urllib.urlretrieve(dic.get("play_path"), '/users/zhangxianjin/Downloads/sound/%s.m4a' % dic.get("title"))
 
 if __name__ == '__main__':
-   # get_m4a("http://www.ximalaya.com/10936615/album/7651313/") #晓说2017
-   # get_m4a("http://www.ximalaya.com/5286989/album/7100190?page=3") #上古医学
-   another("http://www.ximalaya.com/1800244/album/3071659/")#首席医官
+    another("http://www.ximalaya.com/10936615/album/7651313/") #xiaoshuo2017
+    another("http://www.ximalaya.com/5286989/album/7100190") #shanggu
+    # another("http://www.ximalaya.com/1800244/album/3071659/")#shouxi
+    another("http://www.ximalaya.com/56313539/album/4903380/")#action
+    another("http://www.ximalaya.com/5286989/album/11497918/")#youdao
+    another("http://www.ximalaya.com/5286989/album/10114187/")#sushi
